864261fd7ff1190f2cdb000633e9e9421927e0c3
Andrew Tu <andrewtu335@gmail.com>
2025-04-17 22:31:31 -0400

    basic project setup

---
+++ comission/src/main/java/com/comission/comission/CoMissionApplication.java
@@ -0,0 +1,13 @@
+package com.comission.comission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoMissionApplication.class, args);
+	}
+
+}
---
+++ comission/src/test/java/com/comission/comission/CoMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comission.comission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
