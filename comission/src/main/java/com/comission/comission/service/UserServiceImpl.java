e3017048d948dbfe44a3b961b64a65ec7faf7512
Andrew Tu <andrewtu335@gmail.com>
2025-04-28 17:02:07 -0400

    modify login feature to return JWT access and refresh token

--- comission/src/main/java/com/comission/comission/service/UserServiceImpl.java
+++ comission/src/main/java/com/comission/comission/service/UserServiceImpl.java
@@ -3,64 +3,31 @@ package com.comission.comission.service;
 import com.comission.comission.model.User;
 import com.comission.comission.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserServiceImpl implements UserDetailsService {
 
-    private UserRepository userRepository;
-    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-    private AuthenticationManager authManager;
-    private JWTService jwtService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserService(
-            UserRepository userRepository,@Lazy AuthenticationManager authManager,JWTService jwtService)
+    public UserServiceImpl(UserRepository userRepository)
     {
         this.userRepository=userRepository;
-        this.authManager=authManager;
-        this.jwtService=jwtService;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
-    {
-        Optional<User> user = userRepository.findByUserName(username);
-        if(user.isEmpty()){
-            System.out.println("username not found");
-            throw new UsernameNotFoundException("Username is not found");
-        }
-        else {
-            return user.get();
-        }
-    }
-
-    public User register(User user)
-    {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
-    }
-
-    public String verify(User user)
-    {
-        Authentication authentication = authManager.authenticate(
-                new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
-
-        if(authentication.isAuthenticated())
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if(user.isEmpty())
         {
-            return jwtService.generateToken(user.getUsername());
+            throw new UsernameNotFoundException("username is not found");
         }
-
-        return "fail";
+        return user.get();
     }
 }
